496774aaaea725065a4da03d1adf78ad568f1761
BielHtml <gabriel.servulo@aluno.ifsp.edu.br>
2024-10-16 20:25:29 -0300

    Bd salvo

---
+++ bd.sql
@@ -0,0 +1,10 @@
+create database projeto_ajax;
+use projeto_ajax;
+
+create table comments(
+id int(11) auto_increment PRIMARY KEY,
+name_c varchar(100),
+comment_c text
+
+)
+select * from comments
